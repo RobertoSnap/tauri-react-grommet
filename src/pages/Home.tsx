df34dae0686b36d6cb7cbf026e0ef2c5b54d7a2e
robertosnap <robertosnap@pm.me>
2020-05-23 10:12:04 +0200

    Upgrade to 6.2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ export const HomePage: React.FC<Props> = () => {
     return (
         <Box>
             <Heading level={3}>Welcome to Tauri</Heading>
-            <Text >Does this auto reload:1</Text>
+            <Text >Check reload reload:1</Text>
         </Box>
     )
 }
